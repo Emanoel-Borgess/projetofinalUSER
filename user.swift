17e914f7485263bc23142dad555483b2085a1b16
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-02-28 11:48:07 -0300

    Update user.swift

--- user.swift
+++ user.swift
@@ -1,65 +1,70 @@
-//
-//  User.swift
-//  projeto_final01
-//
-//  Created by Turma01-8 on 26/02/25.
-//
-
 import SwiftUI
+struct MenuItem {
+    var icon: String
+    var title: String
+}
 
-struct Chat: View {
+// Estrutura para criar itens do menu clicáveis
+struct UserMenuItem<Destination: View>: View {
+    var icon: String
+    var title: String
+    var destination: Destination
+    
+    var body: some View {
+        NavigationLink(destination: destination) {
+            HStack {
+                Image(systemName: icon)
+                    .foregroundColor(.black)
+                    .frame(width: 30, height: 50)
+                Text(title)
+                    .foregroundColor(.black)
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.black)
+            }
+            .padding(.vertical, 10)
+        }
+    }
+}
+
+struct User: View {
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.white
-                    
                 VStack {
-                    
                     VStack {
-                        VStack {
-                            Image(systemName: "person.crop.circle.fill")
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(.black)
-                            Text("Olá, user!")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                                .foregroundColor(.black)
-                                .padding(.top, 10)
-
-                            
-                        } // Fechamento do HStack
+                        Image(systemName: "person.crop.circle.fill")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.black)
                         
-                    } // Fechamento do VStack
+                        Text("Olá, user!")
+                            .font(.title2)
+                            .fontWeight(.medium)
+                            .foregroundColor(.black)
+                            .padding(.top, 10)
+                    }
                     .frame(maxWidth: .infinity, maxHeight: 350)
-                    .background(.laranja)
+                    .background(Color.orange)
                     .cornerRadius(30.0)
                     .ignoresSafeArea()
-                    
-                    HStack {
-                        VStack {
-                            List {
-                                UserMenuItem(icon: "person.fill", title: "Minha Conta", destination: Home())
-                                UserMenuItem(icon: "calendar", title: "Minhas Sessões", destination: Home())
-                                UserMenuItem(icon: "doc.text.fill", title: "Meus Planos", destination: Home())
-                                UserMenuItem(icon: "leaf.fill", title: "Planos de cuidados", destination: Home())
-                                UserMenuItem(icon: "heart.fill", title: "Meus Favoritos", destination: Home())
-                            }
-                            .listStyle(PlainListStyle())
-                            .frame(maxWidth: .infinity, maxHeight: 450)
-                            .scrollContentBackground(.hidden)
-                        }
+                    List {
+                        UserMenuItem(icon: "person.fill", title: "Minha Conta", destination: Home())
+                        UserMenuItem(icon: "calendar", title: "Minhas Sessões", destination: Home())
+                        UserMenuItem(icon: "doc.text.fill", title: "Meus Planos", destination: Home())
+                        UserMenuItem(icon: "leaf.fill", title: "Planos de cuidados", destination: Home())
+                        UserMenuItem(icon: "heart.fill", title: "Meus Favoritos", destination: Home())
                     }
-                } // Fechamento do VStack
-                
-            }// Fechamento do ZStack
-            
-        }// Fechamento do NavigationStack
+                    .listStyle(PlainListStyle())
+                    .frame(maxWidth: .infinity, maxHeight: 550)
+                    .scrollContentBackground(.hidden)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    Chat()
+    User()
 }
-
-
